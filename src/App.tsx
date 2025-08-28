e91d6aade304d5deb2c9b99328847f32e0d7c6fe
francisquitu91 <riddimrootsAI@gmail.com>
2025-08-28 10:18:30 -0400

    Remove iframe section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import AboutSection from './components/about/AboutSection';
 import ServicesSection from './components/services/ServicesSection';
 import HistorySection from './components/history/HistorySection';
 import { ClientsSection } from './components/clients/ClientsSection';
-import IframeSection from './components/iframe/IframeSection';
 import { QuoteSection } from './components/quote/QuoteSection';
 import IntroScreen from './components/intro/IntroScreen';
 import { Footer } from './components/footer/Footer';
@@ -24,7 +23,6 @@ function App() {
           <ServicesSection />
           <HistorySection />
           <ClientsSection />
-          <IframeSection />
           <QuoteSection />
         </main>
         <Footer />
